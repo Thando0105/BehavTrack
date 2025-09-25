a5e9776e0bdbedc8ac00ed13e8eda559fa96b730
Marlon Demas <marlon@specno.com>
2025-09-25 13:44:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7265769213-24d96",
+  "appId": "1:47909566429:web:b0f73ed24809de8e55a834",
+  "apiKey": "AIzaSyCowPO0mMSHpmnDTdYUl_lCY1BQwiV9Wx4",
+  "authDomain": "studio-7265769213-24d96.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "47909566429"
+};
